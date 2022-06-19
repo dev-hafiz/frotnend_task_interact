05943b1d032b0d68296318d098d4289ee4da3598
okshi <johnstarhafiz@gmail.com>
2022-06-19 12:48:30 +0600

    Complete the header section with custom  Sass style without raw css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
   return (
     <div className="header">
-      
+      <div className="header__content">
+        <h1 className="header__content-h1">
+          Get ready and start to take your <br />
+          career to the next level!
+        </h1>
+        <div className="header__inputArea">
+          <input className="header__inputArea-child1" type="text" placeholder="Your name*" />
+          <input className="header__inputArea-child2" type="email" placeholder="Your email address*" />
+          <button type="submit" className="header__inputArea-child3">Start Free Course</button>
+        </div>
+        <p className="header__content-p">
+          *Sign up and start learning your first course for free!
+        </p>
+      </div>
     </div>
   );
 };
