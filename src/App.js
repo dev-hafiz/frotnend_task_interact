9a0d98e0aedf8f9a8b07d3522db836cf0d96e2f9
okshi <johnstarhafiz@gmail.com>
2022-06-19 11:02:28 +0600

    finshed sass different different file setup and working on header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './sass/main.scss';
+import Header from "./components/Header";
+import "./sass/main.scss";
 
 function App() {
   return (
     <div>
-      
+      <Header />
     </div>
   );
 }
